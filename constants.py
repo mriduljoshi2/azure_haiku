7ba3119eba12535ad62a81252d6f6a57c41055f1
mriduljoshi2 <mridul.joshi@covlant.ai>
2024-11-19 16:56:54 +0530

    remove unused constants

--- constants.py
+++ constants.py
@@ -5,6 +5,3 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 AZURE_API_VERSION = os.getenv("AZURE_API_VERSION", "")
-api_key = os.getenv("AZURE_OPENAI_API_KEY", "")
-deployment_name = "your-deployment-name"
-openai_api_base = os.getenv("AZURE_OPENAI_ENDPOINT", "")
